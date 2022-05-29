189ff96523dbfe6ebd86f17d7057a73e1b83ee61
DanArmor <danilamorozov07@mail.ru>
2022-05-29 17:42:14 +0300

    redesign

--- include/turing.hpp
+++ include/turing.hpp
@@ -2,21 +2,31 @@
 #ifndef _INC_TURING_HPP
 #define _INC_TURING_HPP
 
-#include <vector>
-#include <string>
+#include <map>
+#include <set>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
-enum TuringDirection{
-    Left,
-    Right,
-    NoMove
-};
+enum TuringDirection { Left, Right, NoMove };
 
-struct TuringTurn{
-    TuringTurn() : oldState(-1), oldSymbol('_'), newState(-1), newSymbol('_'), direction(NoMove){}
-    TuringTurn(int oldState, char oldSymbol, int newState, char newSymbol, TuringDirection direction) : 
-    oldState(oldState), oldSymbol(oldSymbol), newState(newState), newSymbol(newSymbol), direction(direction){}
-    ~TuringTurn(){}
+const char LAMBDA = 'λ';
+
+struct TuringTurn {
+    TuringTurn()
+        : oldState(-1),
+          oldSymbol(LAMBDA),
+          newState(-1),
+          newSymbol(LAMBDA),
+          direction(NoMove) {}
+    TuringTurn(int oldState, char oldSymbol, int newState, char newSymbol,
+               TuringDirection direction)
+        : oldState(oldState),
+          oldSymbol(oldSymbol),
+          newState(newState),
+          newSymbol(newSymbol),
+          direction(direction) {}
+    ~TuringTurn() {}
     int oldState;
     char oldSymbol;
     int newState;
@@ -24,90 +34,71 @@ struct TuringTurn{
     TuringDirection direction;
 };
 
-class TuringTape{
-    public:
-    TuringTape(){}
-    TuringTape(std::string tape) : tape(tape){}
-    ~TuringTape(){}
+class TuringTape {
+   public:
+    TuringTape() {}
+    ~TuringTape() {}
 
-    void setChar(int pos, char c){
-        if(pos < 0 || pos >= tape.size())
-            throw std::invalid_argument("Выход за пределы ленты");
-        tape[pos] = c;
+    void setChar(int pos, char c) { tape[pos] = c; }
+
+    char getChar(int pos) {
+        if (tape.count(pos) == 0) {
+            return '\0';
+        } else {
+            return tape[pos];
+        }
     }
 
-    char getChar(int pos){
-        if(pos < 0 || pos >= tape.size())
-            throw std::invalid_argument("Выход за пределы ленты");
-        return tape[pos];
-    }
+    std::map<int, char> getTapeMap(void) { return tape; }
 
-    std::string getStr(void){
-        return tape;
-    }
-
-    private:
-    std::string tape;
+   private:
+    std::map<int, char> tape;
     int offset;
 };
 
-struct TuringState{
-    TuringState(){}
-    ~TuringState(){}
-    std::string alph;
-    std::vector<std::vector<TuringTurn>> table;
+struct TuringState {
+    TuringState() {}
+    ~TuringState() {}
     TuringTape tape;
-    std::string comment;
     int position;
     int currState;
 };
 
-class TuringMachine{
-    public:
-    TuringMachine(){}
-    ~TuringMachine(){}
-    void loadState(TuringState newState){ state = newState; }
+class TuringMachine {
+   public:
+    TuringMachine() {}
+    ~TuringMachine() {}
+    void loadState(TuringState newState) { state = newState; }
 
-    int indexInAlph(char c){
-        for(int i = 0; i < state.alph.size(); i++)
-            if(state.alph[i] == c)
-                return i;
-        throw std::invalid_argument("Символ не входит в алфавит! " + std::to_string(c));
-    }
+    char getCurChar(void) { return state.tape.getChar(state.position); }
 
-    char getCurChar(void){
-        return state.tape.getChar(state.position);
-    }
-
-    void makeTurn(void){
-        if(stop){
+    void makeTurn(void) {
+        if (stop) {
             return;
         }
-        TuringTurn turn = state.table[state.currState][indexInAlph(getCurChar())];
+        TuringTurn turn = table[std::make_pair(state.currState, getCurChar())];
         state.tape.setChar(state.position, turn.newSymbol);
-        if(turn.direction == Left){
+        if (turn.direction == Left) {
             state.position--;
-        } else if (turn.direction == Right){
+        } else if (turn.direction == Right) {
             state.position++;
         }
         state.currState = turn.newState;
-        if(state.currState == -1){
+        if (state.currState == -1) {
             stop = true;
         }
     }
 
-    std::string getTapeStr(void){
-        return state.tape.getStr();
-    }
+    std::string getTapeStr(void) { return state.tape.getStr(); }
 
-    void clear(void){
-        stop = false;
-    }
+    void clear(void) { stop = false; }
 
-    private:
+   private:
+    std::map<std::pair<int, char>, TuringTurn> table;
+    std::set<char> alph;
     bool stop = false;
+    std::string comment;
     TuringState state;
 };
-
 
 #endif
