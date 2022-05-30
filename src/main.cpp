f302f44f356b3f964c776869c2804d3d83573bd2
DanArmor <danilamorozov07@mail.ru>
2022-05-30 18:13:48 +0300

    Пофиксил ширину клеток

--- src/main.cpp
+++ src/main.cpp
@@ -18,23 +18,11 @@ using namespace ftxui;
 const int sizeCells = 20;
 
 int main() {
-    TuringState state{
-        {0, 0},
-        {
-            {0, '0'},
-            {1, '0'},
-            {2, '0'},
-            {3, '_'},
-            {4, '_'}
-        }
-    };
+    TuringState state{{0, 0},
+                      {{0, '0'}, {1, '0'}, {2, '0'}, {3, '_'}, {4, '_'}}};
 
     TuringMachine control{
-        {
-            {{0, '0'}, {0, '1', Right}},
-            {{0, '_'}, {-1, '_', NoMove}}
-        }
-    };
+        {{{0, '0'}, {0, '1', Right}}, {{0, '_'}, {-1, '_', NoMove}}}};
     control.setAlph("01_");
     control.setComment("Комментарий");
 
@@ -52,10 +40,11 @@ int main() {
             cells_strs.push_back("");
         }
         for (int i = 0; i < sizeCells; i++) {
-            cells.push_back(Input(&cells_strs[i], "  "));
+            cells.push_back(Input(&cells_strs[i], ""));
             cells_numbers.push_back(text(std::to_string(i)));
             cells_vbox.push_back(
-                vbox({cells_numbers[i], separator(), cells[i]->Render()}));
+                vbox({cells_numbers[i], separator(), cells[i]->Render()}) |
+                size(WIDTH, Constraint::GREATER_THAN, 3));
             cells_final.push_back(hbox(cells_vbox[i], separator()));
         }
         // return hbox(cells_final);
@@ -64,7 +53,8 @@ int main() {
     auto getFinal = [&]() {
         for (int i = 0; i < sizeCells; i++) {
             cells_vbox[i] =
-                vbox({cells_numbers[i], separator(), cells[i]->Render()});
+                vbox({cells_numbers[i], separator(), cells[i]->Render()})  |
+                size(WIDTH, Constraint::GREATER_THAN, 3);
             cells_final[i] = hbox(cells_vbox[i], separator());
         }
         return cells_final;
