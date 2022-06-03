2c8e5bfafa5d206a393e0af52be8cd61d42e4e2d
DanArmor <danilamorozov07@mail.ru>
2022-06-03 20:14:59 +0300

    Переход на классы

---
+++ include/turingUI.hpp
@@ -0,0 +1,70 @@
+#ifndef _INC_TURING_UI_H
+#define _INC_TURING_UI_H
+
+#include <string>
+
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/dom/table.hpp"
+#include "ftxui/screen/screen.hpp"
+#include "ftxui/screen/string.hpp"
+#include "ftxui/util/ref.hpp"
+
+#include "turing.hpp"
+
+class TuringTableUI : public ftxui::ComponentBase{
+    private:
+    public:
+    TuringTableUI();
+    ftxui::Element Render() override;
+    bool OnEvent(ftxui::Event) override;
+};
+
+class TuringTapeUI : public ftxui::ComponentBase{
+    private:
+    int size;
+    int leftIndex;
+    std::vector<std::wstring> tapeStrs;
+    std::vector<ftxui::Component> tapeInputs;
+
+    int toLocalPos(int pos);
+    bool isValidPos(int pos);
+    
+    public:
+    TuringTapeUI(int size_);
+
+    void setChar(wchar_t c, int pos);
+
+    ftxui::Element Render() override;
+    bool OnEvent(ftxui::Event) override;
+};
+
+class TuringUI : public ftxui::ComponentBase {
+   private:
+    std::function<void()> quit;
+    TuringState state;
+    TuringMachine machine;
+
+    bool isRunning = false;
+    std::wstring alphStr, commentStr;
+
+    ftxui::Component stepButton;
+    ftxui::Component runButton;
+    ftxui::Component alphInput;
+    ftxui::Component commentInput;
+
+    ftxui::Component addButton;
+    ftxui::Component removeButton;
+
+    std::shared_ptr<TuringTapeUI> tapeComponent;
+    std::shared_ptr<TuringTableUI> tableComponent;
+
+   public:
+    TuringUI(std::function<void()> quitFunc);
+
+    ftxui::Element Render() override;
+    bool OnEvent(ftxui::Event) override;
+};
+
+#endif
---
+++ src/turingUI.cpp
@@ -0,0 +1,87 @@
+#include "turingUI.hpp"
+
+// TABLE UI
+TuringTableUI::TuringTableUI() {}
+
+ftxui::Element TuringTableUI::Render(){
+    return ftxui::hbox({});
+}
+
+bool TuringTableUI::OnEvent(ftxui::Event){
+    return false;
+}
+
+// TAPE UI
+TuringTapeUI::TuringTapeUI(int size_) {
+    size = size_;
+}
+
+bool TuringTapeUI::isValidPos(int pos){
+    return pos >= leftIndex && pos < leftIndex+size;
+}
+
+int TuringTapeUI::toLocalPos(int pos){
+    return pos - leftIndex;
+}
+
+void TuringTapeUI::setChar(wchar_t c, int pos){
+    if(!isValidPos(pos))
+        return;
+    pos = toLocalPos(pos);
+    tapeStrs[pos] = c;
+}
+
+ftxui::Element TuringTapeUI::Render(){
+    return ftxui::hbox({});
+}
+
+bool TuringTapeUI::OnEvent(ftxui::Event){
+    return false;
+}
+
+// MAIN UI
+TuringUI::TuringUI(std::function<void()> quitFunc) {
+    quit = quitFunc;
+
+    stepButton = ftxui::Button("Step", [&]() {});
+    runButton = ftxui::Button("Run", [&]() {});
+    alphInput = ftxui::Input(&alphStr, "");
+    commentInput = ftxui::Input(&commentStr, "");
+
+    addButton = ftxui::Button("Add", [&]() {});
+    removeButton = ftxui::Button("Remove", [&]() {});
+
+    tapeComponent = ftxui::Make<TuringTapeUI>(20);
+    tableComponent = ftxui::Make<TuringTableUI>();
+}
+
+ftxui::Element TuringUI::Render(){
+    auto buttonsTape = ftxui::hbox({
+        stepButton->Render(),
+        runButton->Render(),
+        alphInput->Render(),
+        commentInput->Render()
+    });
+
+    auto buttonsTable = ftxui::hbox({
+        addButton->Render(),
+        removeButton->Render()
+    });
+
+    auto mainBox = ftxui::vbox({
+        ftxui::text(L"Машина Тьюринга"),
+        ftxui::separatorHeavy(),
+        tableComponent->Render(),
+        buttonsTape,
+        buttonsTable
+    }) | ftxui::border;
+
+    return mainBox;
+}
+
+bool TuringUI::OnEvent(ftxui::Event event){
+    if(event == ftxui::Event::Character(L'q') || event == ftxui::Event::Escape){
+        quit();
+    }
+    return false;
+}
